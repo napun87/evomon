e6b8b4309c805ba16260c40c64de24f62c1d766b
napun87 <accmaruthunhat@gmail.com>
2026-07-09 00:09:04 +0700

    Update pululite.lua

--- pululite.lua
+++ pululite.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/349171199feeb2b561597b018bf12e5d.lua"))()
+loadstring(game:HttpGet("https://rubuscriptblox.com/script/loader.php?script=pulutee"))()
